1deae7711c32ea83ea2ec1f068338d86f4af5e67
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-02-01 20:45:05 -0500

    SQRT(x)

---
+++ 69.Sqrt(x).py
@@ -0,0 +1,58 @@
+# https://leetcode.com/problems/sqrtx/?envType=featured-list&envId=top-interview-questions%3FenvType%3Dfeatured-list&envId=top-interview-questions
+
+# Sqrt(x)
+# Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+# The returned integer should be non-negative as well.
+# You must not use any built-in exponent function or operator.
+
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+        
+        
+        
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        
+        
+
+
+
+
+
+
+# Test
+# sol = Solution()
+# x1 = 4
+# print(sol.mySqrt(x1));
+
+# sol = Solution()
+# x2 = 8
+# print(sol.mySqrt(x2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
