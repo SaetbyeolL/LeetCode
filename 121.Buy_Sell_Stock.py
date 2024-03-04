4cc75c4667b5e51666d2a0343c6c9719c386e2d0
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-04 15:14:40 -0500

    Best Time to Buy and Sell Stock

--- 121.Buy_Sell_Stock.py
+++ 121.Buy_Sell_Stock.py
@@ -4,18 +4,41 @@
 # you want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
-from typing import List
 
+from typing import List
+# 1st solution
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
         
+        max_profit = 0
+        min_price = prices[0]                         # initialize min_price as 1st index of prices
+        
+        for price in prices:
+            min_price = min(min_price, price)         # compare current price and min_price
+            cur_profit = price - min_price            
+            max_profit = max(max_profit, cur_profit)
+        
+        return max_profit
 
 
+# 2nd solution
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+        
+        for cur_price in prices:
+            if min_price > cur_price:
+                min_price = cur_price
+            profit = cur_price - min_price
+            if profit > max_profit:
+                max_profit = profit
+        
+        return max_profit
 
-
-
-
-
+            
 # Test
 sol = Solution()
 prices1 = [7,1,5,3,6,4]
