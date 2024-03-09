601b83785dccf92d4b150f997806cad73c3e2815
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-09 16:20:57 -0500

    Intersection of Two Linked Lists

--- 160.Intersect_LinkedLists.py
+++ 160.Intersect_LinkedLists.py
@@ -10,6 +10,37 @@ class ListNode:
         self.val = x
         self.next = None
 
+# solution1
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        lenA, lenB = 0, 0                    # length of each linked list's node
+        nodeA, nodeB = headA, headB          # first node
+        
+        while nodeA:
+            lenA += 1
+            nodeA = nodeA.next
+        while nodeB:
+            lenB += 1
+            nodeB = nodeB.next
+            
+        nodeA, nodeB = headA, headB          # reset firstNode
+        
+        # Move the longer list forward by the difference in length between the two lists to align them at the same position
+        while lenA > lenB:                   
+            nodeA = nodeA.next
+            lenA -= 1
+        while lenB > lenA:
+            nodeB = nodeB.next
+            lenB -= 1
+            
+        while nodeA != nodeB:                # find intersection
+            nodeA = nodeA.next
+            nodeB = nodeB.next
+            
+        return nodeA              
+    
+    
+# solution2   
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
         if not headA or not headB:
@@ -22,17 +53,8 @@ class Solution:
             pointerA = pointerA.next if pointerA else headB # When the pointer reaches the end, it moves to the beginning of the opposite list.
             pointerB = pointerB.next if pointerB else headA
         
-        return pointerA # returns the intersection point
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        return pointerA # returns the intersection point    
+              
     
         
 # Test
@@ -41,14 +63,14 @@ sol = Solution()
 intersected_node = ListNode(8)
 intersected_node.next = ListNode(4)
 intersected_node.next.next = ListNode(5)
-listA = ListNode(4)
-listA.next = ListNode(1)
-listA.next.next = intersected_node
-listB = ListNode(5)
-listB.next = ListNode(6)
-listB.next.next = ListNode(1)
-listB.next.next.next = intersected_node
-print(sol.getIntersectionNode(listA, listB))
+headA = ListNode(4)
+headA.next = ListNode(1)
+headA.next.next = intersected_node
+headB = ListNode(5)
+headB.next = ListNode(6)
+headB.next.next = ListNode(1)
+headB.next.next.next = intersected_node
+print(sol.getIntersectionNode(headA, headB))
 
 
 
