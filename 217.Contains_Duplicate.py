1d4efcde6308153e1a2dac6330f158a215a7750d
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-03-18 11:55:49 -0400

    Contains Duplicate

--- 217.Contains_Duplicate.py
+++ 217.Contains_Duplicate.py
@@ -8,20 +8,24 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         container = set()
-
-
-
-
-
+        
+        for i in nums:
+            if i in container:
+                return True
+            else:
+                container.add(i)
+                
+        return False
+    
 
 # Test
 sol = Solution()
 nums1 = [1,2,3,1]
 print(sol.containsDuplicate(nums1))
 nums2 = [1,2,3,4]
-print(sol.containsDuplicate(nums1))
+print(sol.containsDuplicate(nums2))
 nums3 = [1,1,1,3,3,4,3,2,4,2]
-print(sol.containsDuplicate(nums1))
+print(sol.containsDuplicate(nums3))
 
 
 
