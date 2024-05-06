212570847410dfa8cb3e6307b21cb562b0cf6e0e
Saetbyeol Lim <saetbyeol701@gmail.com>
2024-05-06 14:40:19 -0400

    Longest Palindromic Substring

---
+++ 5.Palindromic_Substring.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/longest-palindromic-substring/description/?envType=featured-list&envId=top-interview-questions?envType=featured-list&envId=top-interview-questions
+
+# Given a string 's', return the longest palindromic substring in s.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+
+
+
+
+
+
+
+
+
+
+# Test
+sol = Solution()
+s1 = "babad"
+print(sol.longestPalindrome(s1))
+s2 = "cbbd"
+print(sol.longestPalindrome(s2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
